030e5c88bd87212e67a74745c374f33c9d53298d
XiaoyiWang98 <seanwang20121997@hotmail.com>
2020-04-09 23:34:59 -0400

    function part complete

--- app/main.py
+++ app/main.py
@@ -14,111 +14,222 @@ import cv2
 import threading
 import datetime
 
+
 @webapp.route('/')
 def main():
+    # temp for user login
     return render_template("main.html")
-    
-@webapp.route('/uploader', methods = ['GET', 'POST'])
+
+
+"""
+The main page for video uploading and history checking
+"""
+
+
+@webapp.route('/uploader', methods=['GET', 'POST'])
 def video_uploader():
     if request.method == 'POST':
-       global user_id
-       user_id = str(request.form['UserID'])
-       print(user_id)
-       filename = str(int(time.time()))+'.mp4'
-       url = 'http://0.0.0.0:5000/Video_upload_action'
-       #url = 'https://ax7l11065f.execute-api.us-east-1.amazonaws.com/dev/Video_upload_action'
-       fields = {'success_action_redirect': url}
-       response = S3.create_presigned_post('a3video', filename, fields={'success_action_redirect':url}, conditions=[fields])
-       print(response)
-       return render_template("video_upload.html",response = response, url = url)
+        global user_id
 
-@webapp.route('/Video_upload_action', methods = ['GET', 'POST'])
+        # Get user_id
+        user_id = str(request.form['UserID'])
+
+        # set a storage address for the new image
+        filename = user_id + "/" + str(int(time.time())) + '.mp4'
+
+        # redirect address for local
+        url = 'http://0.0.0.0:5000/Video_upload_action'
+        # redirect address for lambda
+        # url = 'https://ax7l11065f.execute-api.us-east-1.amazonaws.com/dev/Video_upload_action'
+
+        # set redirect address for S3
+        fields = {'success_action_redirect': url}
+
+        # Get a presigned post info
+        response = S3.create_presigned_post('a3video', filename, fields={'success_action_redirect': url},
+                                            conditions=[fields])
+
+        return render_template("video_upload.html", response=response, url=url)
+
+
+"""
+The page for user to config the video analyzer
+"""
+
+
+@webapp.route('/Video_upload_action', methods=['GET', 'POST'])
 def upload_file():
-   if request.method == 'GET':
-      global filename, video_period,fps
-      filename = request.args.get('key')
-      timestamp = int(filename.split('.')[0])
-      time_d = datetime.datetime.fromtimestamp(timestamp)
-      bucket_name = 'a3video'
-      video_period,fps = vp.video_info(bucket_name,filename)
-      format_period = str(datetime.timedelta(seconds=video_period))
-      init_interval = max(int(video_period/10),1)
-      return render_template("video_config.html", tim = time_d, Video_length = format_period, init_interval = str(init_interval), video_period = str(video_period))
+    if request.method == 'GET':
+        # Get all the global objects
+        global filename, video_period, fps, user_id, filedir
 
-@webapp.route('/Video_config_confirm', methods = ['GET', 'POST'])
+        # Get the file_dir of the uploaded file
+        filedir = request.args.get('key')
+
+        # get the filename
+        filename = filedir.split('/')[1]
+
+        # Get the timestamp of uploading, which is the filename
+        timestamp = int(filename.split('.')[0])
+
+        # format the timestamp
+        time_d = datetime.datetime.fromtimestamp(timestamp)
+
+        # Get the info of the uploaded video
+        bucket_name = 'a3video'
+        video_period, fps = vp.video_info(bucket_name, filedir)
+        format_period = str(datetime.timedelta(seconds=video_period))
+        init_interval = max(int(video_period / 10), 1)
+
+        # render the website
+        return render_template("video_config.html", tim=time_d, Video_length=format_period,
+                               init_interval=str(init_interval), video_period=str(video_period))
+
+
+"""
+The page for user to confirm the configuration of the video analyzer
+"""
+
+
+@webapp.route('/Video_config_confirm', methods=['GET', 'POST'])
 def confirm_config():
-   if request.method == 'POST':
-      global filename, real_filename, video_period, interval, start_time, plotx, ploty, image_count, start_flag
-      start_flag = False
-      start = request.form['start-time']
-      Date = (start.split('T')[0]).split('-')
-      tim = (start.split('T')[1]).split(':')
-      start_t = datetime.datetime(year=int(Date[0]),month=int(Date[1]),day=int(Date[2]),hour=int(tim[0]),minute=int(tim[1])).timestamp()
-      start_time = int(start_t)
-      start = datetime.datetime.fromtimestamp(start_time)
-      interval = float(request.form['interval'])
-      processing_time = (video_period/interval)*15
-      format_processing_time = str(datetime.timedelta(seconds=processing_time))
-      return render_template("config_confirm.html", start_time = start, time = format_processing_time)
+    if request.method == 'POST':
+        # Get global variables
+        global filename, real_filename, video_period, interval, start_time, plotx, ploty, image_count, start_flag, user_id
 
-@webapp.route('/Video_processing_start', methods = ['GET', 'POST'])
+        # Set start_flag to False to show that the video hasn't been processed yet
+        start_flag = False
+
+        # Get the start time of the video in date_time
+        start = request.form['start-time']
+        date = (start.split('T')[0]).split('-')
+        tim = (start.split('T')[1]).split(':')
+        start_t = datetime.datetime(year=int(date[0]), month=int(date[1]), day=int(date[2]), hour=int(tim[0]),
+                                    minute=int(tim[1])).timestamp()
+        start_time = int(start_t)
+        start = datetime.datetime.fromtimestamp(start_time)
+
+        # Get the setting of the interval length
+        interval = float(request.form['interval'])
+
+        # Calculate the estimation of the processing time
+        processing_time = (video_period / interval) * 15
+        format_processing_time = str(datetime.timedelta(seconds=processing_time))
+
+        # Render the confirmation page
+        return render_template("config_confirm.html", start_time=start, time=format_processing_time)
+
+
+"""
+Start the video analyzer and get the result from the Dynamodb
+Notice: The video_number_processing will sample frames from the video according to the interval length in user's setup.
+The analyzer will be triggered by file creation in S3 ended with "_unprocessed.jpg". The analyzer is a lambda function
+The result of the analyzer will be written to the Dynamodb
+"""
+
+
+@webapp.route('/Video_processing_start', methods=['GET', 'POST'])
 def video_processing_start():
-   if request.method == 'GET':
-      global filename, filedir, real_filename, video_period, interval, start_time, plotx, ploty, image_count, start_flag, fps
-      if start_flag == False:
-         vp.video_number_processing(filename, 'a3video', interval=interval, options=1, begin_timestamp=start_time)
-         start_flag = True
-      start_time, counters_int,finish_counters, finish_details = ds.get_start_count_finish('temp',filename.split('.')[0])
-      time_line = {}
-      time_count = {}
-      notice_count = {}
-      count = 0
-      counters_int_new = [str(index) for index in counters_int]
-      for key in sorted(finish_counters.keys()):
-         count += 1
-         frame_time = int(start_time) + int(int(key) / fps)
-         time_line[str(key)] = (str(datetime.datetime.fromtimestamp(int(frame_time))))
-         time_count[str(key)] = finish_counters[key]
+    # http get
+    if request.method == 'GET':
 
-      for key in sorted(finish_details.keys()):
-         notice = ""
-         details = finish_details[key]
-         if int(details['person_num']) != 0:
-            Not_Wearing = False
-            Smoke = False
-            for person in details['person_info']:
-               if person['face_mask'] == 'Not Wearing':
-                  Not_Wearing = True
-               if person['smoke'] == 'Smoking':
-                  Smoke = True
-            if Not_Wearing == True:
-               notice = notice + "Not Wearing Face Mask"
-            if Smoke == True:
-               if notice == "":
-                  notice = notice + "Smoking"
-               else:
-                  notice = notice + ", Smoking"
-         if notice == "":
-            notice = "Everything is fine"
-         notice_count[str(key)] = notice
-      if count != len(counters_int_new):
-         finish = False
-      else:
-         finish = True
-      return render_template("video_output.html", len = count, time_line = time_line, time_count = time_count, finish = finish, counts = counters_int_new, notice_count = notice_count)
+        # Set up global variables
+        global filename, filedir, real_filename, video_period, interval, start_time, plotx, ploty, image_count, start_flag, fps, user_id
 
-@webapp.route('/Video_detail', methods = ['GET', 'POST'])
+        # if the video hasn't been sampled yet
+        print(filedir)
+        if start_flag == False:
+            # sample the video and save frames
+            vp.video_number_processing(filedir, user_id, 'a3video', interval=interval, options=1, begin_timestamp=start_time)
+            # set start_flag to be true
+            start_flag = True
+
+        # get the processed info of the video
+        start_time, counters_int, finish_counters, finish_details = ds.get_start_count_finish(user_id,
+                                                                                              filename.split('.')[0])
+
+        # init parameters
+        time_line = {}
+        time_count = {}
+        notice_count = {}
+        count = 0
+
+        # put all the samples frames index to strings
+        counters_int_new = [str(index) for index in counters_int]
+
+        # Get the people counts for each sample frame
+        for key in sorted(finish_counters.keys()):
+            count += 1
+            frame_time = int(start_time) + int(int(key) / fps)
+            time_line[str(key)] = (str(datetime.datetime.fromtimestamp(int(frame_time))))
+            time_count[str(key)] = finish_counters[key]
+
+        # Get the detail information for each sample frame
+        for key in sorted(finish_details.keys()):
+            notice = ""
+            details = finish_details[key]
+            if int(details['person_num']) != 0:
+                Not_Wearing = False
+                Smoke = False
+                for person in details['person_info']:
+                    if person['face_mask'] == 'Not Wearing':
+                        Not_Wearing = True
+                    if person['smoke'] == 'Smoking':
+                        Smoke = True
+                if Not_Wearing == True:
+                    notice = notice + "Not Wearing Face Mask"
+                if Smoke == True:
+                    if notice == "":
+                        notice = notice + "Smoking"
+                    else:
+                        notice = notice + ", Smoking"
+            if notice == "":
+                notice = "Everything is fine"
+            notice_count[str(key)] = notice
+
+        # if the completed information number doesn't equal to the required information number
+        if count != len(counters_int_new):
+            finish = False
+        else:
+            finish = True
+
+        # Render the information page for the video
+        return render_template("video_output.html", len=count, time_line=time_line, time_count=time_count,
+                               finish=finish, counts=counters_int_new, notice_count=notice_count)
+
+
+"""
+Detail information for a single frame
+"""
+
+
+@webapp.route('/Video_detail', methods=['GET', 'POST'])
 def video_details():
-   if request.method == 'POST':
-      global filename
-      image_count = int(request.form['image_name'])
-      timestamp = filename.split('.')[0]
-      image_addr_s = 'temp/'+timestamp + "/%d_unprocessed.jpg" % image_count
-      image_addr = "https://a3video.s3.amazonaws.com/"+ image_addr_s
-      finish_addr_s = 'temp/' + timestamp + "/%d_processed.jpg" % image_count
-      finish_addr = "https://a3video.s3.amazonaws.com/" + finish_addr_s
-      start_time, counters_int, finish_counters, finish_details = ds.get_start_count_finish('temp', filename.split('.')[0])
-      processed_response = finish_details[str(image_count)]
-      ip.image_labeling(image_addr_s,processed_response['person_info'], finish_addr_s)
-      return render_template("video_detail.html", processed_address = finish_addr, person_num = processed_response['person_num'], person_list=processed_response['person_info'])
+    if request.method == 'POST':
+        global filename, user_id
 
+        # Get the clicked image index
+        image_count = int(request.form['image_name'])
+
+        # the timestamp of the video
+        timestamp = filename.split('.')[0]
+
+        # the address of the unprocessed frame
+        image_addr_s = user_id + '/' + timestamp + "/%d_unprocessed.jpg" % image_count
+
+        # the address of the future processed frame
+        finish_addr_s = user_id + '/' + timestamp + "/%d_processed.jpg" % image_count
+        finish_addr = "https://a3video.s3.amazonaws.com/" + finish_addr_s
+
+        # Get the detail information of that frame
+        start_time, counters_int, finish_counters, finish_details = ds.get_start_count_finish(user_id,
+                                                                                              filename.split('.')[0])
+        processed_response = finish_details[str(image_count)]
+
+        # Label the target on the frame
+        ip.image_labeling(image_addr_s, processed_response['person_info'], finish_addr_s)
+
+        # Render the detailed frames
+        return render_template("video_detail.html", processed_address=finish_addr,
+                               person_num=processed_response['person_num'],
+                               person_list=processed_response['person_info'])
--- video_processing.py
+++ video_processing.py
@@ -8,6 +8,7 @@ import boto3
 import dynamodb_service as ds
 import matplotlib.pyplot as plt
 import os
+
 """
 processing a video file given a filename and a interval in seconds
 
@@ -20,80 +21,88 @@ begin_timestamp (optional):
   start time of the video
 """
 
-def video_info(bucket_name,keyname):
-    if S3.file_setpublic(bucket_name,keyname):
-        vim = cv2.VideoCapture('https://'+bucket_name+'.s3.amazonaws.com/'+keyname)
+
+def video_info(bucket_name, keyname):
+    if S3.file_setpublic(bucket_name, keyname):
+        vim = cv2.VideoCapture('https://' + bucket_name + '.s3.amazonaws.com/' + keyname)
         fps = vim.get(cv2.CAP_PROP_FPS)
         period = vim.get(cv2.CAP_PROP_FRAME_COUNT) / fps
-    if S3.file_setprivate(bucket_name,keyname):
-        return period,fps
+    if S3.file_setprivate(bucket_name, keyname):
+        return period, fps
 
-def video_number_processing(file_name,bucket_name, interval, options, begin_timestamp=-1):
-    file_dir = 'https://'+bucket_name+'.s3.amazonaws.com/'+file_name
+
+def video_number_processing(filedir_s,user_id, bucket_name, interval, options, begin_timestamp=-1):
+    file_dir = 'https://' + bucket_name + '.s3.amazonaws.com/' + filedir_s
     image_addr = []
-    if S3.file_setpublic(bucket_name,file_name):
-        vidcap = cv2.VideoCapture(file_dir)
+    if S3.file_setpublic(bucket_name, filedir_s):
+        vid = cv2.VideoCapture(file_dir)
         count = 0
-        fps = vidcap.get(cv2.CAP_PROP_FPS)
-        period = vidcap.get(cv2.CAP_PROP_FRAME_COUNT) / fps
+        fps = vid.get(cv2.CAP_PROP_FPS)
+        period = vid.get(cv2.CAP_PROP_FRAME_COUNT) / fps
         print("Video started at " + str(datetime.fromtimestamp(int(begin_timestamp))))
         interval_frames = interval * fps
-        timestamp = file_name.split('.')[0]
+
+        # get the filename
+        filename = filedir_s.split('/')[1]
+        timestamp = filename.split('.')[0]
         samples = [int(index * interval_frames) for index in range(int(period / interval))]
-        ds.put_user_video('temp', file_name.split('.')[0], samples, {},{}, begin_timestamp)
+        ds.put_user_video(user_id, filename.split('.')[0], samples, {}, {}, begin_timestamp)
+
         while True:
-          success, image = vidcap.read()
-          if not success:
-            break
-          if count % interval_frames == 0:
-            image_addr.append(count)
-            S3.upload_cvimage(image, "%d_unprocessed.jpg" % count, file_directory='temp'+"/"+timestamp + "/")
-          count += 1
-    if S3.file_setprivate(bucket_name,file_name):
+            success, image = vid.read()
+            if not success:
+                break
+            if count % interval_frames == 0:
+                image_addr.append(count)
+                S3.upload_cvimage(image, "%d_unprocessed.jpg" % count, file_directory=user_id + "/" + timestamp + "/")
+            count += 1
+    if S3.file_setprivate(bucket_name, filedir_s):
         return
 
-def video_processing(file_name, interval, options, end_timestamp = -1):
 
-  vidcap = cv2.VideoCapture(file_name)
-  count = 0
-  fps = vidcap.get(cv2.CAP_PROP_FPS)
-  period = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)/fps
-  begin_timestamp = end_timestamp-period
-  print("Video started at "+str(datetime.fromtimestamp(int(begin_timestamp))))
-  interval_frames = interval * fps
-  plotx = []
-  ploty = []
-  while True:
-    success, image = vidcap.read()
-    if not success:
-      break
-    if count%interval_frames == 0:
-      cv2.imwrite("temp/frame%d.jpg" % count, image)  # save frame as JPEG file
-      if options == 1:
-        img_64 = IP.cv_to_64(image)
-        people_num = BP.people_counting(img_64)[0]
-        frame_time = begin_timestamp+count/fps
-        print("At "+ str(datetime.fromtimestamp(int(frame_time))) + ", there are "+ str(people_num)+" people")
-        plotx.append(str(datetime.fromtimestamp(int(frame_time)).time()))
-        ploty.append(int(people_num))
-      elif options == 2:
-        img_64 = IP.cv_to_64(image)
-        people_num, img_out = BP.people_counting(img_64, image=True)
-        frame_time = begin_timestamp+count/fps
-        print("At "+ str(datetime.fromtimestamp(int(frame_time))) + ", there are "+ str(people_num)+" people")
-        cv2.imwrite("temp/frame%d_processed.jpg" % count, img_out)
-        detail_num, dic_people = BP.people_details(img_64)
-        print("Able to analysis "+str(detail_num)+" people in detail")
-        inner_count = 0
-        for people in dic_people:
-            inner_count += 1
-            print("\tperson: " + str(inner_count))
-            attrs = people["attributes"]
-            for attr in attrs:
-                print("\t \t"+attr + ": " + attrs[attr]['name'])
-            print()
-    count += 1
-  plt.title(label=file_name + " on " +str(datetime.fromtimestamp(int(begin_timestamp)).date()))
-  plt.plot(plotx, ploty)
-  plt.axis((min(plotx), max(plotx), 0, max(ploty)*1.25))
-  plt.savefig("processed_plot.png")
+def video_processing(file_name, interval, options, end_timestamp=-1):
+    vidcap = cv2.VideoCapture(file_name)
+    count = 0
+    fps = vidcap.get(cv2.CAP_PROP_FPS)
+    period = vidcap.get(cv2.CAP_PROP_FRAME_COUNT) / fps
+    begin_timestamp = end_timestamp - period
+    print("Video started at " + str(datetime.fromtimestamp(int(begin_timestamp))))
+    interval_frames = interval * fps
+    plotx = []
+    ploty = []
+    while True:
+        success, image = vidcap.read()
+        if not success:
+            break
+        if count % interval_frames == 0:
+            cv2.imwrite("temp/frame%d.jpg" % count, image)  # save frame as JPEG file
+            if options == 1:
+                img_64 = IP.cv_to_64(image)
+                people_num = BP.people_counting(img_64)[0]
+                frame_time = begin_timestamp + count / fps
+                print(
+                    "At " + str(datetime.fromtimestamp(int(frame_time))) + ", there are " + str(people_num) + " people")
+                plotx.append(str(datetime.fromtimestamp(int(frame_time)).time()))
+                ploty.append(int(people_num))
+            elif options == 2:
+                img_64 = IP.cv_to_64(image)
+                people_num, img_out = BP.people_counting(img_64, image=True)
+                frame_time = begin_timestamp + count / fps
+                print(
+                    "At " + str(datetime.fromtimestamp(int(frame_time))) + ", there are " + str(people_num) + " people")
+                cv2.imwrite("temp/frame%d_processed.jpg" % count, img_out)
+                detail_num, dic_people = BP.people_details(img_64)
+                print("Able to analysis " + str(detail_num) + " people in detail")
+                inner_count = 0
+                for people in dic_people:
+                    inner_count += 1
+                    print("\tperson: " + str(inner_count))
+                    attrs = people["attributes"]
+                    for attr in attrs:
+                        print("\t \t" + attr + ": " + attrs[attr]['name'])
+                    print()
+        count += 1
+    plt.title(label=file_name + " on " + str(datetime.fromtimestamp(int(begin_timestamp)).date()))
+    plt.plot(plotx, ploty)
+    plt.axis((min(plotx), max(plotx), 0, max(ploty) * 1.25))
+    plt.savefig("processed_plot.png")
