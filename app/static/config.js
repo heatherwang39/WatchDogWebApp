71a2c745b232aa06bda2e5484197ab170e13726f
Zoey Zhou <55807723+WeekSix@users.noreply.github.com>
2020-04-12 10:14:48 -0400

    UI Design Add html page function

--- app/static/config.js
+++ app/static/config.js
@@ -2,6 +2,6 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_0jjbVupLN',
         region: 'us-east-1',
-		clientId: '1eov611niga5sbrphdnboeoo3c'
-    },
+		clientId: '3knv8vuu03nee5h4fa7j71tp31'
+    }
 };
